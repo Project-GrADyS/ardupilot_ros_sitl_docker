50a9c41d149d882bea7dce3f1d16b0a32029335c
Johan Rocha <johanmarcos50@gmail.com>
2024-02-11 04:34:32 -0300

    creating and compiling packages

--- ardupilot_ros_docker/init.sh
+++ ardupilot_ros_docker/init.sh
@@ -11,38 +11,47 @@ read_ini() {
     echo "$value"
 }
 
-
-
-
 # Lendo e usando os valores do arquivo INI
-PACKAGES=$(read_ini "Configs" "PACKAGES")
+PACKAGES_TO_CREATE=$(read_ini "Configs" "PACKAGES_TO_CREATE")
+PACKAGES_TO_BUILD=$(read_ini "Configs" "PACKAGES_TO_BUILD")
 NODES=$(read_ini "Configs" "NODES_FOR_RUN")
 COMPILE_STATE=$(read_ini "Configs" "COMPILE_STATE")
+CREATE_PACKAGE_STATE=$(read_ini "Configs" "CREATE_PACKAGE_STATE")
 
 source /opt/ros/humble/setup.bash
+mkdir -p /ros2_ws/src
 cd /ros2_ws/src
 
 #no lugar do git clone, devera conter um laco "for" de nome de importacoes dos pacotes
 #git clone https://github.com/ros/ros_tutorials.git -b humble
 #git clone https://github.com/ros-geographic-info/geographic_info.git
+if [ $CREATE_PACKAGE_STATE -eq 1 ]; then
+    echo "*****************CREATING PACKAGES*****************"
+    for package_name in $PACKAGES_TO_CREATE; do
+        echo CREATING $package_name in $(pwd) folder
+        ros2 pkg create --build-type ament_python --license Apache-2.0 $package_name
+    done
+fi
 cd ..
-#rosdep update && rosdep install -i --from-path src --rosdistro humble -y #deve ser colocado
 
 # aqui devera ter um laco "for" com o nome dos pacotes a serem compilados
-#if [ $COMPILE_STATE -eq 1 ]; then
-#    for package_name in $PACKAGES; do
-#        colcon build --packages-up-to $package_name --symlink-install
-#    done
-#fi
+if [ $COMPILE_STATE -eq 1 ]; then
+    echo "*****************BUILDING PACKAGES*****************"
+    rosdep update && rosdep install -i --from-path src --rosdistro humble -y
+    for package_name in $PACKAGES_TO_BUILD; do
+        echo BUILDING $package_name
+        colcon build --packages-up-to $package_name --symlink-install
+    done
+fi
 # --packages-up-to builds the package you want, plus all its dependencies, but not the whole workspace (saves time)
 # --symlink-install saves you from having to rebuild every time you tweak python scripts
 
-#aqui devera rodar o mavros(unstable)
+source /opt/ros/humble/setup.bash
 
+#aqui devera rodar o mavros(unstable)
+ros2 launch mavros apm.launch fcu_url:=tcp://sitl_1:5760
 
 #aqui devera ter um laco "for" que ativara os conjuntos de nodes que irao rodar em ordem
 #for node in $NODES; do
 #    ros2 run $node
 #done
-
-ros2 launch mavros apm.launch fcu_url:=tcp://sitl_1:5760
--- scripts/arm_test.py
+++ scripts/arm_test.py
@@ -2,3 +2,5 @@
 
 import rclpy
 from rclpy.node import Node
+
+from sensor_msgs.msg import Image
