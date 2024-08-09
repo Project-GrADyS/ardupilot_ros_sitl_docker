4d09a65061a03ea35a9036bdeccb1bf4f1034f41
Johan Rocha <johanmarcos50@gmail.com>
2024-08-09 01:35:25 -0300

    feat(#10): lazy initialization for to increase performance

---
+++ ardupilot_ros_docker/ros2_workspace/src/drone_basics/drone_basics/ArdupilotModules/utils.py
@@ -0,0 +1,13 @@
+# utils.py
+
+#for lazy initialization
+def lazy_property(fn):
+    attr_name = '_lazy_' + fn.__name__
+
+    @property
+    def _lazy_property(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+    
+    return _lazy_property
