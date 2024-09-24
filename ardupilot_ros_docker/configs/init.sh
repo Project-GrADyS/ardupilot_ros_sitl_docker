a170eab0615325a91d5f67e116befe0923a2dd43
Johan Rocha <johanmarcos50@gmail.com>
2024-09-24 16:48:19 -0300

    feat: serial interface

--- ardupilot_ros_docker/configs/init.sh
+++ ardupilot_ros_docker/configs/init.sh
@@ -6,13 +6,13 @@ source constants.sh
 read_ini() {
     local section=$1
     local key=$2
-    local value=$(awk -F '=' '/\['$section'\]/{f=1} f==1&&$1~/'$key'/{print $2;exit}' $CONFIG_FILE)
+    local value=$(awk -F '=' '/\['$section'\]/{f=1} f==1&&$1~/'$key'/{print $2;exit}' "$CONFIG_FILE")
     echo "$value"
 }
 
 # Função para criar vetor de subvetores
 create_subvectors() {
-    local nodes="$1"  # Recebe a string de entrada como argumento
+    local nodes="$1" # Recebe a string de entrada como argumento
     echo "$nodes" | awk '{ for (i=1; i<=NF; i+=2) printf "\"%s %s\" ", $i, $(i+1) }'
 }
 
@@ -23,6 +23,7 @@ NODES_FOR_RUN=$(read_ini "Configs" "NODES_FOR_RUN")
 COMPILE_STATE=$(read_ini "Configs" "COMPILE_STATE")
 CREATE_PACKAGE_STATE=$(read_ini "Configs" "CREATE_PACKAGE_STATE")
 COM_ARCH=$(read_ini "Configs" "COM_ARCH")
+FCU_URL=$(read_ini "Configs" "FCU_URL")
 
 nodes_run_array=$(create_subvectors "$NODES_FOR_RUN")
 eval "nodes_run_array=($nodes_run_array)"
@@ -34,7 +35,7 @@ cd /ros2_ws/src
 #no lugar do git clone, devera conter um laco "for" de nome de importacoes dos pacotes
 #git clone https://github.com/ros/ros_tutorials.git -b humble
 #git clone https://github.com/ros-geographic-info/geographic_info.git
-if [ $CREATE_PACKAGE_STATE -eq 1 ]; then
+if [ "$CREATE_PACKAGE_STATE" -eq 1 ]; then
     echo -e "${RED} \
     ********************************** \
     CREATING PACKAGES \
@@ -48,17 +49,17 @@ cd ..
 
 function build_package() {
     echo -e "${RED}BUILDING ${GREEN_BOLD}$1"
-    colcon build --packages-up-to $1 --symlink-install
+    colcon build --packages-up-to $1 --cmake-clean-cache
     source install/local_setup.bash
 }
 
 # aqui devera ter um laco "for" com o nome dos pacotes a serem compilados
-if [ $COMPILE_STATE -eq 1 ]; then
+if [ "$COMPILE_STATE" -eq 1 ]; then
     echo -e "${RED} \
     ********************************** \
     BUILDING PACKAGES \
     **********************************"
-    rosdep update && rosdep install -i --from-path src --rosdistro humble -y
+    rosdep update && rosdep install -i --from-paths src --ignore-src --rosdistro humble -y
     for package_name in $PACKAGES_TO_BUILD; do
         build_package $package_name
     done
@@ -68,14 +69,14 @@ fi
 
 source /opt/ros/$ROS_DISTRO/setup.bash
 
-if [ $COM_ARCH -eq 1 ]; then
+if [ "$COM_ARCH" -eq 1 ]; then
     echo -e "${GREEN_BOLD}INITIALIZING-MODE:\
     ${RED}EProsima Micro XRCE DDS Client"
     if ! [ -d "./src/micro_ros_setup" ]; then
 
-        git clone -b $ROS_DISTRO \
-        https://github.com/micro-ROS/micro_ros_setup.git \
-        src/micro_ros_setup
+        git clone -b "$ROS_DISTRO" \
+            https://github.com/micro-ROS/micro_ros_setup.git \
+            src/micro_ros_setup
 
         apt update && rosdep update
         rosdep install --from-paths src --ignore-src -y
@@ -88,24 +89,24 @@ if [ $COM_ARCH -eq 1 ]; then
 
     source /ros2_ws/install/local_setup.bash
     cd /
-#in development
+    #in development
     if [ ! -d "ardupilot" ]; then
         git clone --depth=1 https://github.com/ArduPilot/ardupilot.git
         cd /ardupilot || exit
         git sparse-checkout set --no-cone libraries/AP_DDS/
         cd /ardupilot/libraries/AP_DDS/ || exit
     fi
-#export PATH=$PATH:/dds-gen/scripts
+    #export PATH=$PATH:/dds-gen/scripts
     ros2 run micro_ros_agent micro_ros_agent udp4 --port 144540 --verbose 6
 
 fi
 #https://discuss.ardupilot.org/t/testing-ppp-and-dds-on-a-desktop-or-laptop/120187
 #aqui devera rodar o mavros(unstable)
-if [ $COM_ARCH -eq 2 ]; then
+if [ "$COM_ARCH" -eq 2 ]; then
     echo -e "${GREEN_BOLD}INITIALIZING MODE:\
     ${RED}MAVROS"
-    ros2 launch mavros apm.launch fcu_url:=udp://0.0.0.0:14540@14540 tgt_system:=1 &
-fi 
+    ros2 launch mavros apm.launch fcu_url:="$FCU_URL" tgt_system:=1 &
+fi
 #fcu_url:=udp://sitl_1:14551@14551 tgt_system:=1
 #fcu_url:=tcp://sitl_1:5760 tgt_system:=1
 
